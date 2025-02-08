1e383652be3375be50eb6b677eadaa40d526c2c0
Katoragashua <katoragashua@outlook.com>
2025-02-08 05:21:42 +0100

    Audiophile: Fixed bug relating to image paths

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,15 +4,15 @@ import CategoryCard from "./CategoryCard";
 const categories = [
   {
     category: "headphones",
-    img: "/src/assets/shared/desktop/image-category-thumbnail-headphones.png",
+    img: "src/assets/shared/desktop/image-category-thumbnail-headphones.png",
   },
   {
     category: "speakers",
-    img: "/src/assets/shared/desktop/image-category-thumbnail-speakers.png",
+    img: "src/assets/shared/desktop/image-category-thumbnail-speakers.png",
   },
   {
     category: "earphones",
-    img: "/src/assets/shared/desktop/image-category-thumbnail-earphones.png",
+    img: "src/assets/shared/desktop/image-category-thumbnail-earphones.png",
   },
 ];
 
